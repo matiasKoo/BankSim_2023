b2b4c757774b8b32985651bd43ca6d5925c1b952
Matias Karjalainen <t1kama00@students.oamk.fi>
2023-04-02 19:04:45 +0300

    webtoken login korttinumerolla ja PINillä

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const jwt = require('jsonwebtoken');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var tiliRouter = require('./routes/tili');
 var asiakasRouter = require('./routes/asiakas');
 var korttiRouter = require('./routes/kortti');
+var loginRouter = require('./routes/login');
 
 
 var app = express();
@@ -18,11 +20,37 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Suojaamattomat endpointit
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/login',loginRouter);
+
+app.use(authenticateToken); //Tämä middlewarena estää alemmat routet ilman oikeaa tokenia
+
+//Suojatut endpointit
 app.use('/tili', tiliRouter);
 app.use('/asiakas', asiakasRouter);
 app.use('/kortti', korttiRouter);
+
+//Pekan esimerkistä funktio https://peatutor.com/express/Examples/webtoken.php
+//Autentikoi siis saamansa tokenin, käytetään middlewarena
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+  
+    console.log("token = "+token);
+    if (token == null) return res.sendStatus(401)
+  
+    jwt.verify(token, process.env.MY_TOKEN, (err, user) => {
+      console.log(err)
+  
+      if (err) return res.sendStatus(403)
+  
+      req.user = user
+  
+      next()
+    })
+}
 
 
 module.exports = app;
--- backend/models/kortti_model.js
+++ backend/models/kortti_model.js
@@ -1,4 +1,7 @@
 const db = require('../database');
+const bcrypt = require('bcryptjs');
+
+const saltRounds=10;
 
 const kortti = {
   getAll: function(callback) {
@@ -8,21 +11,26 @@ const kortti = {
     return db.query('select * from kortti where idkortti=?', [id], callback);
   },
   add: function(kortti, callback) {
-    return db.query(
-      'insert into kortti (asiakas_idasiakas,korttinumero,PIN,luottoraja,tili_idtili) values(?,?,?,?,?)',
-      [kortti.asiakas_idasiakas, kortti.korttinumero, kortti.PIN, kortti.luottoraja, kortti.tili_idtili],
-      callback
-    );
+    bcrypt.hash(kortti.PIN, saltRounds, function(err, hash) {
+      return db.query(
+        'insert into kortti (asiakas_idasiakas,korttinumero,PIN,luottoraja,tili_idtili) values(?,?,?,?,?)',
+        [kortti.asiakas_idasiakas, kortti.korttinumero, hash, kortti.luottoraja, kortti.tili_idtili],
+        callback
+      );
+    });
   },
   delete: function(id, callback) {
     return db.query('delete from kortti where idkortti=?', [id], callback);
   },
   update: function(id, kortti, callback) {
-    return db.query(
-      'update kortti set asiakas_idasiakas=?, korttinumero=?, PIN=?, luottoraja=?, tili_idtili=? where idkortti=?',
-      [kortti.asiakas_idasiakas, kortti.korttinumero, kortti.PIN, kortti.luottoraja, kortti.tili_idtili, id],
-      callback
-    );
+
+    bcrypt.hash(kortti.PIN, saltRounds, function(err, hash) {
+      return db.query(
+        'update kortti set asiakas_idasiakas=?, korttinumero=?, PIN=?, luottoraja=?, tili_idtili=? where idkortti=?',
+        [kortti.asiakas_idasiakas, kortti.korttinumero, hash, kortti.luottoraja, kortti.tili_idtili, id],
+        callback
+      );
+    });
   }
 };
 module.exports = kortti;
