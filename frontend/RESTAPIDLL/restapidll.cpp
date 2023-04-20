84545e63ab375e5ee970f410c11d32e6a610e310
JuusoKorpinen <122262588+JuusoKorpinen@users.noreply.github.com>
2023-04-20 15:58:14 +0300

    Lisäyksiä+parannuksia restapiin
    Nyt lähettää signaalina exe:lle saldon, tilitapahtumat ja virheilmoitukset.
    
    Poistin myös vahingossa tänne pistämäni pro.user tiedoston

--- frontend/RESTAPIDLL/restapidll.cpp
+++ frontend/RESTAPIDLL/restapidll.cpp
@@ -29,14 +29,23 @@ void RESTAPIDLL::loginSlot(QNetworkReply *reply)
         qDebug()<<"Login OK";
         token = "Bearer "+response_data;
         qDebug()<<token;
-    }
 
-    reply->deleteLater();
-    accessManager->deleteLater();
-    GetCardInfo();
+        reply->deleteLater();
+        accessManager->deleteLater();
+        getCardInfo();
+
+    }
+    else
+    {
+        qDebug()<<"Login FAILED";
+        emit sendError("false");
+
+        reply->deleteLater();
+        accessManager->deleteLater();
+    }
 }
 
-void RESTAPIDLL::getByCardInfoSlot(QNetworkReply *reply)
+void RESTAPIDLL::getCardInfoSlot(QNetworkReply *reply)
 {
     response_data=reply->readAll();
     qDebug()<<response_data;
@@ -47,10 +56,54 @@ void RESTAPIDLL::getByCardInfoSlot(QNetworkReply *reply)
         QJsonObject json_obj = value.toObject();
         idtili=QString::number(json_obj["tili_idtili"].toInt());
         idasiakas=QString::number(json_obj["asiakas_idasiakas"].toInt());
+        luottoraja=QString::number(json_obj["luottoraja"].toInt());
     }
 
     qDebug()<<"idtili = "<<idtili;
     qDebug()<<"idasiakas = "<<idasiakas;
+    qDebug()<<"luottoraja = "<<luottoraja;
+
+    reply->deleteLater();
+    accessManager->deleteLater();
+    getCustomerInfo();
+}
+
+void RESTAPIDLL::getCustomerInfoSlot(QNetworkReply *reply)
+{
+    response_data=reply->readAll();
+    qDebug()<<response_data;
+
+    QJsonDocument json_doc = QJsonDocument::fromJson(response_data);
+    QJsonObject json_obj = json_doc.object();
+    fname=json_obj["fname"].toString();
+    lname=json_obj["lname"].toString();
+    henkilotunnus=json_obj["henkilotunnus"].toString();
+
+    qDebug()<<"fname = "<<fname;
+    qDebug()<<"lname = "<<lname;
+    qDebug()<<"henkilotunnus = "<<henkilotunnus;
+    reply->deleteLater();
+    accessManager->deleteLater();
+}
+
+void RESTAPIDLL::getTransactionsSlot(QNetworkReply *reply)
+{
+    response_data=reply->readAll();
+    qDebug()<<response_data;
+
+    QJsonDocument json_doc = QJsonDocument::fromJson(response_data);
+    QJsonArray json_array = json_doc.array();
+    foreach (const QJsonValue &value, json_array) {
+        QJsonObject json_obj = value.toObject();
+        QString aika = json_obj["aika"].toString();
+        QString maara = QString::number(json_obj["maara"].toInt());
+        tilitapahtumat = "Aika: "+aika+" | Määrä: "+maara;
+        list<<tilitapahtumat;
+        qDebug()<<list;
+        emit sendTransactions(list);
+
+    }
+
     reply->deleteLater();
     accessManager->deleteLater();
 }
@@ -63,8 +116,8 @@ void RESTAPIDLL::getSaldoSlot(QNetworkReply *reply)
     QJsonDocument json_doc = QJsonDocument::fromJson(response_data);
     QJsonObject json_obj = json_doc.object();
     saldo=QString::number(json_obj["saldo"].toInt());
-
     qDebug()<<"saldo = "<<saldo;
+    emit sendSaldo(saldo);
     reply->deleteLater();
     accessManager->deleteLater();
 }
@@ -73,10 +126,23 @@ void RESTAPIDLL::nostaSlot(QNetworkReply *reply)
 {
     response_data=reply->readAll();
     qDebug()<<response_data;
-    reply->deleteLater();
-    accessManager->deleteLater();
-}
+    if(QString::compare(response_data, "0")!=0 && QString::compare(response_data, "Unauthorized")!=0)
+    {
+        qDebug()<<"Nosto successful";
 
+        reply->deleteLater();
+        accessManager->deleteLater();
+    }
+    else
+    {
+        qDebug()<<"Nosto failed";
+        emit sendError("0");
+
+        reply->deleteLater();
+        accessManager->deleteLater();
+    }
+
+}
 
 void RESTAPIDLL::Login()
 {
@@ -95,7 +161,7 @@ void RESTAPIDLL::Login()
     reply = accessManager->post(request, QJsonDocument(jsonObj).toJson());
 }
 
-void RESTAPIDLL::GetCardInfo()
+void RESTAPIDLL::getCardInfo()
 {
     QString site_url = Environment::getBaseUrl()+"/kortti/1/"+cardNumber;
     QNetworkRequest request((site_url));
@@ -105,7 +171,37 @@ void RESTAPIDLL::GetCardInfo()
 
     accessManager = new QNetworkAccessManager(this);
 
-    connect(accessManager, SIGNAL(finished(QNetworkReply*)), this, SLOT(getByCardInfoSlot(QNetworkReply*)));
+    connect(accessManager, SIGNAL(finished(QNetworkReply*)), this, SLOT(getCardInfoSlot(QNetworkReply*)));
+
+    reply = accessManager->get(request);
+}
+
+void RESTAPIDLL::getCustomerInfo()
+{
+    QString site_url = Environment::getBaseUrl()+"/asiakas/"+idasiakas;
+    QNetworkRequest request((site_url));
+    //WEBTOKEN ALKU
+    request.setRawHeader(QByteArray("Authorization"),(token));
+    //WEBTOKEN LOPPU
+
+    accessManager = new QNetworkAccessManager(this);
+
+    connect(accessManager, SIGNAL(finished(QNetworkReply*)), this, SLOT(getCustomerInfoSlot(QNetworkReply*)));
+
+    reply = accessManager->get(request);
+}
+
+void RESTAPIDLL::getTransactions()
+{
+    QString site_url = Environment::getBaseUrl()+"/tilitapahtumat/1/"+idtili;
+    QNetworkRequest request((site_url));
+    //WEBTOKEN ALKU
+    request.setRawHeader(QByteArray("Authorization"),(token));
+    //WEBTOKEN LOPPU
+
+    accessManager = new QNetworkAccessManager(this);
+
+    connect(accessManager, SIGNAL(finished(QNetworkReply*)), this, SLOT(getTransactionsSlot(QNetworkReply*)));
 
     reply = accessManager->get(request);
 }
